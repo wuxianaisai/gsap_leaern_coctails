1b43dfe42a4400aa2cd5f97877967dd71cc70d69
wuxianaisai <wuxianaisai@mail.ru>
2026-03-23 11:19:05 +0300

    adding a video scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 import { useGSAP } from "@gsap/react"
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+
     useGSAP(() => {
         const heroSplit = new SplitText(".title", { type: "chars, words" });
         const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -34,7 +40,27 @@ const Hero = () => {
         })
             .to(".right-leaf", { y: 200 }, 0)
             .to(".left-leaf", { y: -200 }, 0)
+
+        const startValue = isMobile ? "top 50%" : "center 60%";
+        const endValue = isMobile ? "120% top" : "bottom top";
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        })
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration
+            })
+        }
     }, []);
+
     return (
         <>
             <section id="hero" className="noisy">
@@ -57,7 +83,7 @@ const Hero = () => {
                         <div className="space-y-5 hidden md:block">
                             <p>Cool. Crisp. Classic.</p>
                             <p className="subtitle">
-                                Sip the Spirit <br /> if Summer
+                                Sip the Spirit <br /> of Summer
                             </p>
                         </div>
                         <div className="view-cocktails">
@@ -70,6 +96,16 @@ const Hero = () => {
                     </div>
                 </div>
             </section>
+
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    src="/videos/output.mp4"
+                    muted
+                    playsInline
+                    preload="auto"
+                />
+            </div>
         </>
     )
 }
